83d1fce3c6e0d3cb19f8814f8ec8f891cb29a885
dmaish <mainadaniel81@gmail.com>
2016-07-23 16:05:20 +0300

    first commit

---
+++ kindargaten/scriptings/main.js
@@ -0,0 +1,12 @@
+$().ready(function(){
+$(".triangle").click(function(){
+$(".triangle").css("background-color","orange");
+});
+
+
+$().click(function(){
+$(".circle").click(functions(){ 
+    $(".circle").css("background-color","blue");                  
+}); 
+    
+});
